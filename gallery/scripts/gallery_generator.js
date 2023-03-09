9571c1431c5c8c20634f728a823d9e43120053f2
szuyun <sl798@sussex.ac.uk>
2023-03-09 12:16:43 +0000

    feat: p28_step3_Creating_the_javascript_controller

---
+++ gallery/scripts/gallery_generator.js
@@ -0,0 +1,30 @@
+var xmlHttp = new XMLHttpRequest();
+var numberOfColumns = 4;
+var htmlCode = "";
+var response;
+
+$(document).ready(function () {
+    var send = "./gallery/scripts/hooks.php";
+    xmlHttp.open("GET", send, true);
+    xmlHttp.send();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4) {
+            //response handler code
+            alert(xmlHttp.responseText);
+            response = xmlHttp.responseText.split("~");
+            htmlCode += "<tr>";
+            for (var i = 0; i < response.length; i++) {
+                htmlCode += '<td id="gallery_cell">';
+                htmlCode += '<a href="images/' + response[i] + '">';
+                htmlCode += '<img src="images/' + response[i] + '" id="image_thumbnail"/>';
+                htmlCode += '</a>';
+                htmlCode += '</td>';
+                if ((i + 1) % numberOfColumns == 0) {
+                    htmlCode += "</tr><tr>";
+                }
+            }
+            htmlCode += "</tr>";
+            document.getElementById("gallery").innerHTML = htmlCode;
+        }
+    };
+});
