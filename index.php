d380db52584a1c840dd34a6cb25f7e0157b07f7d
szuyun <sl798@sussex.ac.uk>
2023-03-09 10:27:24 +0000

    chore: prepare gallery files, restructure folders

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="./JS/custom.js"></script>
-  <script src="./JS/swap_restyle.js"></script>
+  <script src="./scripts/js/custom.js"></script>
+  <script src="./scripts/js/swap_restyle.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
   <!-- our project just needs Font Awesome Solid + Brands -->
   <link rel="stylesheet" href="./assets/fontawesome/css/all.css">
