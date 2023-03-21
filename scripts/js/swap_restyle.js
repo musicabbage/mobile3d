32a20f4b0451a0cd7b05bc3c6861f2b6667a5a65
szuyun <sl798@sussex.ac.uk>
2023-03-21 23:04:35 +0000

    feat: update `swap_restyle.js`

--- scripts/js/swap_restyle.js
+++ scripts/js/swap_restyle.js
@@ -1,3 +1,85 @@
+$(document).ready(function() {
+	
+	selectPage();
+	selectModel();
+
+	function selectPage() {
+
+		$('#home').show();
+		$('#about').hide();
+		$('#models').hide();
+		$('#interaction').hide();
+		// $('#cokeDescription').hide();
+		$('#spriteDescription').hide(); 
+		$('#pepperDescription').hide(); 
+
+
+		$('#navHome').click(function(){
+			$('#home').show();
+			$('#about').hide();
+			$('#models').hide();
+			$('#interaction').hide();
+			$('#cokeDescription').hide();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 	  
+		});
+
+		$('#navAbout').click(function(){
+			$('#home').hide();
+			$('#about').show();
+			$('#models').hide();
+			$('#interaction').hide();
+			$('#cokeDescription').hide();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 	  
+		});
+
+		$('#navModels').click(function(){
+			$('#home').hide();
+			$('#about').hide();
+			$('#models').show();
+			$('#interaction').show(); 
+			$('#cokeDescription').show();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 
+		});
+	}
+
+	function selectModel() {
+
+		$('#navCoke').click(function(){
+			$('#coke').show();
+			$('#sprite').hide();
+			$('#pepper').hide();
+			$('#interaction').show(); 
+			$('#cokeDescription').show();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').hide(); 
+		});
+
+		$('#navSprite').click(function(){
+			$('#coke').hide();
+			$('#sprite').show();
+			$('#pepper').hide();
+			$('#interaction').show(); 
+			$('#cokeDescription').hide();
+			$('#spriteDescription').show();
+			$('#pepperDescription').hide();  	  	  
+		});
+
+		$('#navPepper').click(function(){
+			$('#coke').hide();
+			$('#sprite').hide();
+			$('#pepper').show();
+			$('#interaction').show(); 
+			$('#cokeDescription').hide();
+			$('#spriteDescription').hide(); 
+			$('#pepperDescription').show(); 	   
+		});
+	}
+
+});
+
 function swap(selected) {
     document.getElementById('home').style.display = 'none';
     document.getElementById('coke').style.display = 'none';
