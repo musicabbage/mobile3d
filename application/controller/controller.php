4142ca4f5fdfdc759230b325d0aab803732eb188
szuyun <sl798@sussex.ac.uk>
2023-03-30 11:30:11 +0100

    feat: create sqlite file

--- application/controller/controller.php
+++ application/controller/controller.php
@@ -7,13 +7,13 @@ class Controller {
 	public $model;
 	
 	// Create functions for the controller class
-	function __construct() // contructor of the class
+	function __construct($pageURI = null) // contructor of the class
 	{
 		// Create new objects for Load and Model
 		$this->load = new Load(); 
 		$this->model = new Model();
 		// Determine what page you are on
-		$this->home();
+		$this->$pageURI();
 	}
     // home page function
 	function home()
@@ -21,7 +21,42 @@ class Controller {
 		// Get data from the defined model method - model3D_info()
 		$data = $this->model->model3D_info();
 		// Tell the loader what view to load and which dat to use
-		$this->load->view('view3DAppTest', $data);
+		$this->load->view('view3DAppTest_2', $data);
+	}
+
+	// other page functions
+	function apiCreateTable()
+	{
+		return;
+	  	// echo "Create table function";
+		$data = $this->model->dbCreateTable();
+		$this->load->view('viewMessage', $data);
+	}
+	function apiInsertData()
+	{
+		$data = $this->model->dbInsertData();
+	   	$this->load->view('viewMessage', $data);
+	}  
+	function apiGetData()
+	{
+		$data = $this->model->dbGetData();
+		$this->load->view('view3DAppData', $data);
+	}  
+	
+	// db functions
+	function dbCreateTable()
+	{
+		echo "Create Table Function";
+	}
+
+	function dbInsertData()
+	{
+		echo "Data Insert Function";
+	}
+
+	function dbGetData()
+	{
+		echo "Data Read Function";
 	}
 }
 ?>    
--- application/mvc.php
+++ application/mvc.php
@@ -3,6 +3,10 @@
 require 'view/load.php';
 require 'model/model.php';
 require 'controller/controller.php';
-// Initialise by creating a new controller instance (or object)
-new Controller('home');	
+$pageURI =$_SERVER['REQUEST_URI'];
+$pageURI =substr($pageURI,strrpos($pageURI,'index.php')+10);
+	if (!$pageURI)
+		new Controller('home');
+	else
+		new Controller($pageURI);
 ?>
